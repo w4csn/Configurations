8563baad17616360b854bc0a4ba8b68784d1502d
Scott Newton <snewton86@gmail.com>
2020-10-10 13:02:10 -0400

    Updates

--- config/examples/Creality/Ender-5 Plus/BigTreeTech SKR Mini E3 2.0/2.0.6.1/Configuration.h
+++ config/examples/Creality/Ender-5 Plus/BigTreeTech SKR Mini E3 2.0/2.0.6.1/Configuration.h
@@ -736,7 +736,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 93 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.57, 80.80, 800, 103 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.57, 80.80, 800, 143.86 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -785,11 +785,11 @@
  */
 #define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
-  #define DEFAULT_XJERK  8.0
-  #define DEFAULT_YJERK  8.0
+  #define DEFAULT_XJERK  4.0
+  #define DEFAULT_YJERK  4.0
   #define DEFAULT_ZJERK  0.4
 
-  //#define TRAVEL_EXTRA_XYJERK 1.0     // Additional jerk allowance for all travel moves
+  //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
   //#define LIMITED_JERK_EDITING        // Limit edit via M205 or LCD to DEFAULT_aJERK * 2
   #if ENABLED(LIMITED_JERK_EDITING)
     #define MAX_JERK_EDIT_VALUES { 20, 20, 0.6, 10 } // ...or, set your own edit limits
@@ -819,7 +819,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-//#define S_CURVE_ACCELERATION
+#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
